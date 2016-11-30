076273309ce672bec1173520f0d07fe1bccae7f1
Anubhav Singh <anubhavs286@gmail.com>
2016-11-30 03:53:49 +0000

    Created the models and made migrations & database

--- task/blog/models.py
+++ task/blog/models.py
@@ -1,5 +1,22 @@
 from __future__ import unicode_literals
 
 from django.db import models
-
+from model_utils.managers import InheritanceManager
 # Create your models here.
+class Content(models.Model):
+    title=models.CharField(max_length=120)
+    sub_title=models.CharField(max_length=120)
+    
+    objects = InheritanceManager()
+    
+    def __unicode__(self):
+        return self.title + " -- " + self.sub_title
+    
+class Content_Types(Content):
+    content=models.TextField()
+    image=models.ImageField(blank=True)
+    video=models.FileField(blank=True)
+    
+    def __unicode__(self):
+        return self.content
+    
