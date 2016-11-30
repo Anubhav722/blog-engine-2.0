61392e88a56c984c8243d28276250f3d2a920f54
Anubhav Singh <anubhavs286@gmail.com>
2016-11-30 07:47:11 +0000

    Ready to run

--- task/blog/urls.py
+++ task/blog/urls.py
@@ -3,4 +3,7 @@ from blog import views
 
 urlpatterns = [
    url(r'^$', views.index, name='index'),
+   url(r'heading/(?P<title>([\w ]+))$', views.heading, name='heading'),
+   url(r'sub_titles/(?P<sub_title>([\w ]+))$', views.sub_titles, name='sub_titles'),
+   #url(r'^new/(?P<state>([\w ]+))$', views.new_state, name='new_state'),
 ]
--- task/blog/views.py
+++ task/blog/views.py
@@ -1,5 +1,40 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, get_list_or_404, get_object_or_404
+from blog.models import Content, Content_Types
 
+from django.db.models import Count
 # Create your views here.
 def index(request):
-    return HttpResponse("It's working")
+    queryset=Content.objects.all().values_list("title", flat=True).distinct()
+    
+    context={
+        "queryset":queryset,
+    }
+    return render(request, "index.html", context)
+    
+def heading(request, title):
+    
+    instance_list=get_list_or_404(Content, title=title)
+    instance=Content.objects.filter(title=title).values_list("sub_title", flat=True).distinct()
+    #myset=set(instance_list)
+    #print myset
+    context={
+        "title":title,
+        "instance_list":instance_list,
+        "instance":instance
+        
+    }
+    return render(request, "heading.html", context)
+    
+def sub_titles(request, sub_title):
+    
+    instance_list=get_list_or_404(Content, sub_title=sub_title)
+    
+    instance=Content.objects.filter(sub_title=sub_title).select_subclasses()
+    
+   
+    context={
+        "instance":instance,
+        "instance_list":instance_list
+    }
+    return render(request, "detail.html", context)
+    
